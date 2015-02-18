581f1d1a78c2a3d8656129b30ea709220b9b769c
Christian Hoffmeister <mail@choffmeister.de>
2015-02-18 23:02:16 +0100

    Generate search index

--- lib/jekyll_search.rb
+++ lib/jekyll_search.rb
@@ -1,4 +1,7 @@
 require 'jekyll'
+require 'elasticsearch'
+require 'loofah'
+require 'loofah/helpers'
 
 module Jekyll
   module Commands
@@ -17,17 +20,61 @@ module Jekyll
         end
 
         def process(options)
-            options = configuration_from_options(options)
-            destination = options['destination']
+          options = configuration_from_options(options)
+          destination = options['destination']
 
-            site = Jekyll::Site.new(options)
-            settings = site.config['search']
+          site = Jekyll::Site.new(options)
+          site.process
+          settings = site.config['search']
 
-            for page in site.pages
-              puts page.to_s
+          client = Elasticsearch::Client.new log: false
+          client.indices.delete index: 'documentation'
+
+          for page in site.pages
+            body = {
+              url: site.baseurl + page.url,
+              title: page.data['title'],
+              content: clean_content(page.content)
+            }
+
+            client.index index: 'documentation', type: 'page', body: body
+          end
+        end
+
+        def clean_content(dirty)
+          Loofah.fragment(dirty).to_text
+          #.gsub(/([\r\n\t\s]+)/, ' ').strip
+        end
+      end
+    end
+
+    class Search < Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:search) do |c|
+            c.syntax "search"
+            c.description 'Searches a search index in Elasticsearch.'
+
+            c.action do |args, options|
+              options["serving"] = false
+              Search.process(options)
             end
+          end
+        end
 
-            puts settings
+        def process(options)
+          puts 'Enter query string:'
+          query = gets
+
+          client = Elasticsearch::Client.new log: false
+          result = client.search index: 'documentation', body: { query: { match: { content: query } }, highlight: { fields: { content: {} }} }
+
+          puts "Total: #{result['total']}"
+          puts "Max score: #{result['max_score']}"
+          for hit in result['hits']['hits']
+            puts "Hit at #{hit['_source']['url']} (#{hit['_score']})"
+            puts hit['highlight']['content']
+          end
         end
       end
     end
